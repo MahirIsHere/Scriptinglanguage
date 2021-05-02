f2da9ca416d15d5959afc315471ee4493498412b
Mahir Rahman <mahirfaisalv@gmail.com>
2021-05-02 22:03:00 +0600

    Create getElementById.js

---
+++ JavaScript/getElementById.js
@@ -0,0 +1,9 @@
+<html>
+<h1 id = "hi">"hello"</h1> 
+<p id ="text">some text</p> 
+</html>
+
+  <script>
+  document.getElementById('hi').innerHTML="hello world"
+document.getElementById('text').innerHTML="text changed"
+<script>
